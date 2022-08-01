5ca7bccd7d76c0d7bfb92075483fa1c821361346
shotiko13 <shotov1303@gmail.com>
2022-08-01 16:43:34 +0400

    Final commit, everything works.

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.entry, name = 'entry'),
-    path("search", views.search , name = "search "),
+    path("search", views.search , name = "search"),
     path("newpage/" , views.gotoPage, name = "newpage"),
     path("save", views.savepage, name = "save"),
     path("edit", views.editpage, name = "edit"),
-    path("saveedit", views.saveedit, name = "edit"),
-    path("randompage", views.randompage, name = 'random'),
+    path("saveedit", views.saveedit, name = "saveedit"),
+    path("randompage", views.randompage, name = 'randompage'),
 ]
---
+++ entries/Gocha.md
@@ -0,0 +1,3 @@
+#Gocha
+
+abs
